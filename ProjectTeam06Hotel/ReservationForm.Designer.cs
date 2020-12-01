0ed9df956b72a5dc26f3a5d15d259e41d57b0832
Sakshi Prasad <prasads17@student.douglascollege.ca>
2020-11-30 23:32:57 -0800

    Payment form- data displayed -amount not added

--- ProjectTeam06Hotel/ReservationForm.Designer.cs
+++ ProjectTeam06Hotel/ReservationForm.Designer.cs
@@ -35,7 +35,6 @@
             this.labelCheckindate = new System.Windows.Forms.Label();
             this.labelcheckoutDate = new System.Windows.Forms.Label();
             this.labelNumberOfGuests = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
             this.dataGridViewReservations = new System.Windows.Forms.DataGridView();
             this.label9 = new System.Windows.Forms.Label();
             this.buttonBook = new System.Windows.Forms.Button();
@@ -50,6 +49,8 @@
             this.listBoxGuestName = new System.Windows.Forms.ListBox();
             this.listBoxRoomType = new System.Windows.Forms.ListBox();
             this.checkBoxPay = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewReservations)).BeginInit();
             this.SuspendLayout();
             // 
@@ -115,15 +116,6 @@
             this.labelNumberOfGuests.Size = new System.Drawing.Size(120, 17);
             this.labelNumberOfGuests.TabIndex = 6;
             this.labelNumberOfGuests.Text = "Number of guests";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(20, 9);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(120, 17);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "Reservation Form";
             // 
             // dataGridViewReservations
             // 
@@ -239,11 +231,31 @@
             this.checkBoxPay.Text = "Payment made";
             this.checkBoxPay.UseVisualStyleBackColor = true;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 17);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Select Guest Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(124, 17);
+            this.label2.TabIndex = 25;
+            this.label2.Text = "Select Room Type";
+            // 
             // ReservationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(906, 795);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.checkBoxPay);
             this.Controls.Add(this.listBoxRoomType);
             this.Controls.Add(this.listBoxGuestName);
@@ -258,7 +270,6 @@
             this.Controls.Add(this.buttonBook);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.dataGridViewReservations);
-            this.Controls.Add(this.label8);
             this.Controls.Add(this.labelNumberOfGuests);
             this.Controls.Add(this.labelcheckoutDate);
             this.Controls.Add(this.labelCheckindate);
@@ -283,7 +294,6 @@
         private System.Windows.Forms.Label labelCheckindate;
         private System.Windows.Forms.Label labelcheckoutDate;
         private System.Windows.Forms.Label labelNumberOfGuests;
-        private System.Windows.Forms.Label label8;
         private System.Windows.Forms.DataGridView dataGridViewReservations;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Button buttonBook;
@@ -298,5 +308,7 @@
         private System.Windows.Forms.ListBox listBoxGuestName;
         private System.Windows.Forms.ListBox listBoxRoomType;
         private System.Windows.Forms.CheckBox checkBoxPay;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
--- ProjectTeam06Hotel/ReservationForm.cs
+++ ProjectTeam06Hotel/ReservationForm.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using EFControllerUtilities;
+using SeedDatabaseExtensions;
 using VancouverHotelCodeFirstFromDB;
 
 namespace ProjectTeam06Hotel
@@ -24,27 +25,26 @@ namespace ProjectTeam06Hotel
 
             this.Text = "Reservation Form";
             context = new VancouverHotelEntities();
+            context.Database.Log = (s => Debug.Write(s));
+            context.SeedDatabase();
+            context.SaveChanges();
 
             this.Load += ReservationForm_Load;
+
 
             // register event handler for when a guest is selected
             listBoxGuestName.SelectedIndexChanged += (s, e) => GetReservation();
             listBoxRoomType.SelectedIndexChanged += (s, e) => GetReservation();
 
             // always dispose of the context when the form is closed.
-           // this.FormClosed += (s, e) => context.Dispose();
+            // this.FormClosed += (s, e) => context.Dispose();
 
             buttonBook.Click += ButtonBook_Click;
-           // buttonCancelBooking.Click += DeleteBook_Click;
+            // buttonCancelBooking.Click += DeleteBook_Click;
         }
 
         private void ReservationForm_Load(object sender, EventArgs e)
         {
-            context = new VancouverHotelEntities();
-            context.Database.Log = s => Debug.Write(s);
-            context.SaveChanges();
-            context.Reservations.Load();
-            
             // bind the listbox of guest and rooms
 
             listBoxGuestName.DataSource = Controller<VancouverHotelEntities, Guest>.SetBindingList();
@@ -62,7 +62,12 @@ namespace ProjectTeam06Hotel
 
             InitializeDataGridView<Reservation>(dataGridViewReservations, "Reservations");
 
+            context = new VancouverHotelEntities();
+            context.Database.Log = (s => Debug.Write(s));
+            context.Payments.Load();
+            context.SaveChanges();
         }
+
         //select guest and room
         private void GetReservation()
         {
@@ -75,13 +80,18 @@ namespace ProjectTeam06Hotel
                 textBoxRoomID.Text = room.RoomId.ToString();
             }
         }
+
         //book room
         private void ButtonBook_Click(object sender, EventArgs e)
         {
             if (!(listBoxGuestName.SelectedItem is Guest guest))
             {
-                MessageBox.Show("guest to be updated must be selected");
-                return;
+                if (!(listBoxRoomType.SelectedItem is Room room))
+
+                {
+                    MessageBox.Show("guest or room to be updated must be selected");
+                    return;
+                }
             }
             // get the guest data from the textboxes
 
@@ -95,17 +105,6 @@ namespace ProjectTeam06Hotel
             reservation.CheckInDate = DateTime.Parse(textBoxCheckinDate.Text);
             reservation.CheckOutDate = DateTime.Parse(textBoxCheckoutDate.Text);
 
-                
-            
-
-            // validity checks
-
-            //if (reservation.InfoIsInvalid())
-            //{
-               // MessageBox.Show("Guest information is missing.");
-                //return;
-           // }
-
             // now update the db
 
             if (Controller<VancouverHotelEntities, Reservation>.AddEntity(reservation) == null)
@@ -114,14 +113,13 @@ namespace ProjectTeam06Hotel
                 return;
             }
 
-            
-            InitializeDataGridView<Reservation>(dataGridViewReservations, "Reservations");
 
-            
+            InitializeDataGridView<Reservation>(dataGridViewReservations, "Reservation");
+
+
         }
-
-        //inital datagrid
-        private void InitializeDataGridView<T>(DataGridView gridView, params string[] navProperties) where T : class
+            //inital datagrid
+            private void InitializeDataGridView<T>(DataGridView gridView, params string[] navProperties) where T : class
         {
             // Allow users to add/delete rows, and fill out columns to the entire width of the control
 
@@ -129,7 +127,7 @@ namespace ProjectTeam06Hotel
             gridView.AllowUserToDeleteRows = true;
             gridView.ReadOnly = true;
             gridView.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
-           
+
             // set the handler used to delete an item. Note use of generics.
 
             //gridView.UserDeletedRow += GridView_UserDeletedRow<T>;
@@ -157,13 +155,6 @@ namespace ProjectTeam06Hotel
             Debug.WriteLine("DataError " + typeof(T) + " " + gridView.Name + " row " + e.RowIndex + " col " + e.ColumnIndex + " Context: " + e.Context.ToString());
             e.Cancel = true;
         }
-
-
-       
-
-        
-
-
     }
 
 }
